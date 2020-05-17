384d1a8f0019a81c0f8ecac0fb094d3f35e0e716
dayhappy <846031778@qq.com>
2020-05-17 12:36:20 +0800

    Initial commit

---
+++ src/com/syy/Mains.java
@@ -0,0 +1,24 @@
+package com.syy;
+
+
+import java.util.Date;
+import java.util.HashMap;
+
+public class Mains {
+
+    private static int threadlocal = 200;
+    private static int cliendlocal = 5000;
+    public static void main(String[] args) {
+        System.out.println("xxx");
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        Date date = new Date();
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+    }
+    public void nes(){
+
+    }
+}
---
+++ src/com/syy/test.java
@@ -0,0 +1,19 @@
+package com.syy;
+
+/**
+ * @author siyongye
+ *
+ * @create 2020-05-15
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+}
